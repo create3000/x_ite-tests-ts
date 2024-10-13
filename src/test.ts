660f6a05e1e31132eeeb181c5c7bb0595b37b43c
Holger Seelig <holger.seelig@gmail.com>
2024-10-13 14:52:59 +0200

    Updated test.

--- src/test.ts
+++ src/test.ts
@@ -1,8 +1,7 @@
 import X3D from "../../x_ite";
 
-let browser: X3D.X3DBrowser = X3D.getBrowser ();
-
-let scene: X3D.X3DScene = browser .currentScene;
+const browser: X3D.X3DBrowser = X3D.getBrowser ();
+const scene:   X3D.X3DScene   = browser .currentScene;
 
 console .log (scene);
 console .log (X3D .X3DConstants .SFNode);
