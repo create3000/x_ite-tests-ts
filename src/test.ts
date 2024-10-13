6d6d57126cd2e2d08e39af370be0d471432e9172
Holger Seelig <holger.seelig@gmail.com>
2024-10-13 14:52:20 +0200

    Updated test.

--- src/test.ts
+++ src/test.ts
@@ -4,4 +4,6 @@ let browser: X3D.X3DBrowser = X3D.getBrowser ();
 
 let scene: X3D.X3DScene = browser .currentScene;
 
-console .log (scene)
+console .log (scene);
+console .log (X3D .X3DConstants .SFNode);
+console .log (X3D .ProfileInfo);
