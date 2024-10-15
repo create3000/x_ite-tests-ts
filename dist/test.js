5d8a9b142f8b4cb8b6ca2c13f152cda413c0ef4e
Holger Seelig <holger.seelig@gmail.com>
2024-10-15 13:49:31 +0200

    Updated test.

--- dist/test.js
+++ dist/test.js
@@ -1,4 +1,6 @@
 import X3D from "../../x_ite";
-let browser = X3D.getBrowser();
-let scene = browser.currentScene;
+const browser = X3D.getBrowser();
+const scene = browser.currentScene;
 console.log(scene);
+console.log(X3D.X3DConstants.SFNode);
+console.log(X3D.ProfileInfo);
