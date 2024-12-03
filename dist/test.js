31ed96f7f37fd18924a4efa9ff457d1b3472a53e
Holger Seelig <holger.seelig@gmail.com>
2024-12-03 19:09:34 +0100

    Updated test.

--- dist/test.js
+++ dist/test.js
@@ -1,6 +1,11 @@
 import X3D from "../../x_ite";
+import traverse from "../../x3d-traverse";
+const Traverse = traverse(X3D);
 const browser = X3D.getBrowser();
 const scene = browser.currentScene;
 console.log(scene);
 console.log(X3D.X3DConstants.SFNode);
 console.log(X3D.ProfileInfo);
+scene.addRootNode(scene.createNode("Transform"));
+for (const node of scene.traverse(Traverse.ROOT_NODES))
+    console.log(node.getNodeTypeName());
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,7 @@
 import X3D from "../../x_ite";
+import traverse from "../../x3d-traverse";
+
+const Traverse = traverse (X3D);
 
 const browser: X3D.X3DBrowser = X3D.getBrowser ();
 const scene:   X3D.X3DScene   = browser .currentScene;
@@ -6,3 +9,8 @@ const scene:   X3D.X3DScene   = browser .currentScene;
 console .log (scene);
 console .log (X3D .X3DConstants .SFNode);
 console .log (X3D .ProfileInfo);
+
+scene .addRootNode (scene .createNode ("Transform"));
+
+for (const node of scene .traverse (Traverse .ROOT_NODES))
+   console .log ((node as X3D .SFNode) .getNodeTypeName ())
