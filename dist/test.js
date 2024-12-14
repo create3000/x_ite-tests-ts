c8eee4cba51779d200f099444cc461d4354e78be
Holger Seelig <holger.seelig@gmail.com>
2024-12-14 19:19:02 +0100

    Updated deps.

--- dist/test.js
+++ dist/test.js
@@ -1,8 +1,9 @@
-import X3D from "../../x_ite";
-import traverse from "../../x3d-traverse";
+import X3D from "x_ite";
+import traverse from "x3d-traverse";
 const Traverse = traverse(X3D);
 const browser = X3D.getBrowser();
 const scene = browser.currentScene;
+const scene2 = await browser.createScene(browser.getProfile("Core"));
 console.log(scene);
 console.log(X3D.X3DConstants.SFNode);
 console.log(X3D.ProfileInfo);
